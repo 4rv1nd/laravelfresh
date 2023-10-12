8b95eff3c66cbb321b5fd2613d513c8b239586ea
= <=>
2023-10-12 21:06:34 +0530

    fixed table errors

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::view('/', 'welcome');
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+
+    // Artisan::call('cache:clear');
+    // Artisan::call('view:clear');
+    // Artisan::call('route:clear');
+
+    // Artisan::call('route:cache');
+    // Artisan::call('config:cache');
+    return back();
+})->name('clear');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
